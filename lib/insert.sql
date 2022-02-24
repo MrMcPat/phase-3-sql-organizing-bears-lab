553c119544f946578e4829598baca91cdf40d360
Patrick Liang <pl204511194@yahoo.com>
2022-02-24 16:09:13 -0500

    commit tree 5066cc7f7a7f3befb635ecdc0be4a5b8b8e0ee33

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'sexy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 7, 'M', 'red', 'angry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'green', 'sad', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'M', 'brown', 'mad', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 10, 'F', 'pink', 'cool', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 20, 'M', 'green', 'scary', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'M', 'yellow', 'fat', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 999, 'M', 'white', 'idk', 1);
